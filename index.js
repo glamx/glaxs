281b68d7eca781a9459c85bdb6cb1a7f612d7b7c
glamx <glam.v2020@gmail.com>
2022-07-11 23:10:18 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const { Router } = require('express');
+var express = require('express');
+var app = express();
+
+var path = require("path");
+var router = express.Router()
+
+
+
+
+router.get("/", (req, res) => {
+    res.sendFile(path.join( __dirname + "/past/home.html"))
+}) // cria a rota de url
+
+router.get("/contato", (req, res) => {
+    res.sendFile(path.join( __dirname + "/past/contato.html"))
+})
+
+app.use(router) //falar pra aplicacao aplicar o route
+
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log('seerv on')
+})
+
+
+
+
+
+
+
