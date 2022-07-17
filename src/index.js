f5d68a17ca67cd9d94d966bcb486a7320d4485c6
glamx <glam.v2020@gmail.com>
2022-07-17 15:40:37 -0300

    xxsa

--- src/index.js
+++ src/index.js
@@ -9,8 +9,6 @@ var router = express.Router()
 
 
 
-
-
 router.get("/", (req, res) => {
     res.sendFile(path.join( __dirname + "/past/home.html"))
 }) // cria a rota de url
@@ -33,4 +31,14 @@ app.listen(process.env.PORT || 3000, ()=>{
 
 
 
+async function getExternalImage(){
+    var response = await fetch ('https://images.unsplash.com/photo-1657483577058-e51f452120ef?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1ODA4Mjk4Nw&ixlib=rb-1.2.1&q=80&w=1080') 
 
+    var c = document.querySelector('.image')
+
+    c.innerHTML = `<img src="${response.url}" >`
+}
+
+
+
+getExternalImage()
